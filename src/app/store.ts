6ab021f451b16bb1e8ab4dbb2297dcc1d111932d
maherapi <maher.azkoul@gmail.com>
2021-04-15 10:35:40 +0300

    reset state on logout

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,4 +1,4 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import feedbackSlice from "./data-source/feedback/clientFeedbackSlice";
 import authSlice from "./auth/authSlice";
 import registrationSlice from "./registration/registrationSlice";
@@ -6,15 +6,26 @@ import feedbackMiddleware from "./middlewares/feedback.middleware";
 import messagingSlice from "./messaging/messagingSlice";
 import ordersSlice from "./orders/ordersSlice";
 import schoolsSlice from "./schools/schoolsSlice";
+import ringsSlice from "./rings/ringsSlice";
+
+const combinedReducer = combineReducers({
+  feedback: feedbackSlice,
+  auth: authSlice,
+  registration: registrationSlice,
+  messaging: messagingSlice,
+  orders: ordersSlice,
+  schools: schoolsSlice,
+  rings: ringsSlice,
+});
+
+const rootReducer = (state: any, action: any) => {
+  if (action.type === "auth/logout/fulfilled") {
+    state = undefined;
+  }
+  return combinedReducer(state, action);
+};
 
 export const store = configureStore({
-  reducer: {
-    feedback: feedbackSlice,
-    auth: authSlice,
-    registration: registrationSlice,
-    messaging: messagingSlice,
-    orders: ordersSlice,
-    schools: schoolsSlice,
-  },
+  reducer: rootReducer,
   middleware: [feedbackMiddleware],
 });
